de2a2a9ccffc0d23f7b0619227e0e769a9b3d68d
Angelbotics 1339 <72036931+Dresden6@users.noreply.github.com>
2022-10-09 14:36:12 -0600

    updates

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-# import streamlit as st
+import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
@@ -7,15 +7,11 @@ import json
 
 url = "https://data.mongodb-api.com/app/data-eajpu/endpoint/data/v1/action/findOne"
 
-# st.title('New Scouting App')
-
-
-
 payload = json.dumps({
-    "collection": "testData",
-    "database": "Cluster0",
-    "dataSource": "Cluster0",
-    "projection": {}
+  "collection": "testData",
+  "database": "Cluster0",
+  "dataSource": "Cluster0",
+  "projection": {}
 })
 
 headers = {
@@ -27,6 +23,28 @@ headers = {
 
 @st.cache
 def load_data():
-    data = requests.request("POST", url, headers=headers, data=payload)
-    return data.json()
+  data = requests.request("POST", url, headers=headers, data=payload)
+  return data.json()
     
+def main_page():
+  st.markdown("# Main page 🎈")
+  st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+  st.markdown("# Page 2 ❄️")
+  st.sidebar.markdown("# Page 2 ❄️")
+
+page_names_to_funcs = {
+  "Main Page": main_page,
+  "Page 2": page2
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()    
+    
+    
+# TODO
+# Title: 2022-2023 Scout App
+# Sidebar
+# Main page make datavisual
+# Page 2 is input and change name
